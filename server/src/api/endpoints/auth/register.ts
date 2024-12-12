e8a0254be5ee508fded066deb86a002b1e01b62b
Timothy <timothyrwashburn@gmail.com>
2024-12-12 01:29:50 -0800

    trim name and email fields

--- server/src/api/endpoints/auth/register.ts
+++ server/src/api/endpoints/auth/register.ts
@@ -3,8 +3,8 @@ import AuthManager from '../../../controllers/auth-manager';
 import { z } from 'zod';
 
 const registerSchema = z.object({
-    name: z.string().min(1),
-    email: z.string().email(),
+    name: z.string().trim().min(1),
+    email: z.string().trim().email(),
     password: z.string().min(4)
 });
 
